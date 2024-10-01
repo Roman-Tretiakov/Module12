b0cf360aba790df95ae8922642da47d0fb9425b6
Roman_Tretiakov <romantre@wizardsdev.com>
2024-10-02 01:26:55 +0300

    Added HW33

---
+++ tests_12_2.py
@@ -0,0 +1,42 @@
+import runner_and_tournament as rt
+import unittest
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    def setUp(self):
+        self.runner_1 = rt.Runner('Усэйн', 10)
+        self.runner_2 = rt.Runner('Андрей', 9)
+        self.runner_3 = rt.Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        print(*cls.all_results, sep='\n')
+
+    def test_tournament_1(self):
+        _tournament = rt.Tournament(90, self.runner_1, self.runner_3)
+        self.all_results.append(_tournament.start())
+
+        self.assertTrue(self.all_results[-1][2], self.runner_3.name)
+
+    def test_tournament_2(self):
+        _tournament = rt.Tournament(90, self.runner_2, self.runner_3)
+        self.all_results.append(_tournament.start())
+
+        self.assertTrue(self.all_results[-1][2], self.runner_3.name)
+
+    def test_tournament_3(self):
+        _tournament = rt.Tournament(90, self.runner_1, self.runner_2, self.runner_3)
+        self.all_results.append(_tournament.start())
+
+        self.assertTrue(self.all_results[-1][3], self.runner_3.name)
+
+    # Дополнительный тест на правильное расположение бегунов в забегах:
+    def test_tournaments(self):
+        exp_results_tournament = [{1: self.runner_1.name, 2: self.runner_3.name}, {1: self.runner_2.name,
+            2: self.runner_3.name}, {1: self.runner_1.name, 2: self.runner_2.name, 3: self.runner_3.name}]
+
+        self.assertEqual(self.all_results, exp_results_tournament)
